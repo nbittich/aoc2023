e541a92a7605747d183070ff7e854fd91e197da7
Nordine Bittich <nordine@bittich.be>
2023-12-02 12:30:08 +0100

    day2: do both in single fn

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,5 @@
 fn main() -> Result<(), &'static str> {
-    part1().ok_or("error")?;
-    part2().ok_or("error")?;
-    Ok(())
+    run().ok_or("error")
 }
 
 fn position_digit(s: &str) -> Option<usize> {
@@ -12,12 +10,13 @@ fn position_digit(s: &str) -> Option<usize> {
         .last()
 }
 
-fn part1() -> Option<()> {
+fn run() -> Option<()> {
     let input = include_str!("input.txt");
     let max_red_cube = 12;
     let max_green_cube = 13;
     let max_blue_cube = 14;
-    let mut total = 0;
+    let mut total_part1 = 0;
+    let mut total_part2 = 0;
 
     for line in input.lines() {
         let line = &line[5..];
@@ -25,9 +24,11 @@ fn part1() -> Option<()> {
         let id = line[0..=pos_id].to_string().parse::<u32>().ok()?;
         let line = &line[pos_id + 2..];
         let bags = line.split(';');
-
+        let mut min_blue_cube = 1;
+        let mut min_red_cube = 1;
+        let mut min_green_cube = 1;
         let mut valid = true;
-        'inner: for bag in bags {
+        for bag in bags {
             let cubes = bag.split(',');
             for cube in cubes.map(|c| c.trim()) {
                 let pos_amount = position_digit(cube)?;
@@ -37,66 +38,23 @@ fn part1() -> Option<()> {
                     "blue" => {
                         if amount > max_blue_cube {
                             valid = false;
-                            break 'inner;
                         }
-                    }
-                    "red" => {
-                        if amount > max_red_cube {
-                            valid = false;
-                            break 'inner;
-                        }
-                    }
-                    "green" => {
-                        if amount > max_green_cube {
-                            valid = false;
-                            break 'inner;
-                        }
-                    }
-                    _ => {
-                        panic!("something went wrong '{color}'");
-                    }
-                }
-            }
-        }
-        if valid {
-            total += id;
-        }
-    }
-    println!("part1: {total}");
-
-    Some(())
-}
-
-fn part2() -> Option<()> {
-    let input = include_str!("input.txt");
-    let mut total = 0;
-
-    for line in input.lines() {
-        let line = &line[5..];
-        let pos_id = position_digit(line)?;
-        let line = &line[pos_id + 2..];
-        let bags = line.split(';');
-        let mut min_blue_cube = 1;
-        let mut min_red_cube = 1;
-        let mut min_green_cube = 1;
-        for bag in bags {
-            let cubes = bag.split(',');
-            for cube in cubes.map(|c| c.trim()) {
-                let pos_amount = position_digit(cube)?;
-                let amount = cube[0..=pos_amount].to_string().parse::<u32>().ok()?;
-                let color = &cube[pos_amount + 1..];
-                match color.trim() {
-                    "blue" => {
                         if amount > min_blue_cube {
                             min_blue_cube = amount;
                         }
                     }
                     "red" => {
+                        if amount > max_red_cube {
+                            valid = false;
+                        }
                         if amount > min_red_cube {
                             min_red_cube = amount;
                         }
                     }
                     "green" => {
+                        if amount > max_green_cube {
+                            valid = false;
+                        }
                         if amount > min_green_cube {
                             min_green_cube = amount;
                         }
@@ -107,9 +65,14 @@ fn part2() -> Option<()> {
                 }
             }
         }
-        total += min_blue_cube * min_red_cube * min_green_cube;
+        if valid {
+            total_part1 += id;
+        }
+        total_part2 += min_blue_cube * min_red_cube * min_green_cube;
     }
-    println!("part2: {total}");
+
+    println!("part1: {total_part1}");
+    println!("part2: {total_part2}");
 
     Some(())
 }
